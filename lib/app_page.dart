61179e4a96139d7b84eb7c30be2395c0de31f445
Nithin Rauj <nithindrake@gmail.com>
2019-09-15 12:35:57 +0530

    Created reusable widgets for buttons and finished the layout for buttons area

--- lib/app_page.dart
+++ lib/app_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppPage extends StatefulWidget {
   @override
@@ -33,6 +34,7 @@ class _AppPageState extends State<AppPage> {
                           style: TextStyle(
                             fontSize: 50.0,
                             fontWeight: FontWeight.w300,
+                            color: Colors.white54,
                           ),
                         ),
                         Text(
@@ -60,13 +62,158 @@ class _AppPageState extends State<AppPage> {
                     borderRadius: BorderRadius.circular(8.0),
                     color: Colors.black45,
                   ),
-                  child: Text(
-                    'Buttons Area',
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          OperatorButtonWidget(
+                            operator: FontAwesomeIcons.plus,
+                            buttonColor: Colors.grey[800],
+                          ),
+                          OperatorButtonWidget(
+                            operator: FontAwesomeIcons.minus,
+                            buttonColor: Colors.grey[800],
+                          ),
+                          OperatorButtonWidget(
+                            operator: FontAwesomeIcons.times,
+                            buttonColor: Colors.grey[800],
+                          ),
+                          OperatorButtonWidget(
+                            operator: FontAwesomeIcons.divide,
+                            buttonColor: Colors.grey[800],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          NumberButtonWidget(
+                            label: '1',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          NumberButtonWidget(
+                            label: '2',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          NumberButtonWidget(
+                            label: '3',
+                            buttonColor: Colors.grey[900],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          NumberButtonWidget(
+                            label: '4',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          NumberButtonWidget(
+                            label: '5',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          NumberButtonWidget(
+                            label: '6',
+                            buttonColor: Colors.grey[900],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          NumberButtonWidget(
+                            label: '7',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          NumberButtonWidget(
+                            label: '8',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          NumberButtonWidget(
+                            label: '9',
+                            buttonColor: Colors.grey[900],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          OperatorButtonWidget(
+                            operator: FontAwesomeIcons.backspace,
+                            buttonColor: Colors.grey[800],
+                          ),
+                          NumberButtonWidget(
+                            label: '0',
+                            buttonColor: Colors.grey[900],
+                          ),
+                          OperatorButtonWidget(
+                            operator: FontAwesomeIcons.equals,
+                            buttonColor: Colors.redAccent,
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
           ]),
+    );
+  }
+}
+
+class NumberButtonWidget extends StatelessWidget {
+  final String label;
+  final Color buttonColor;
+
+  NumberButtonWidget({@required this.label, this.buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FlatButton(
+          onPressed: () {},
+          color: buttonColor,
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 40.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OperatorButtonWidget extends StatelessWidget {
+  final IconData operator;
+  final Color buttonColor;
+
+  OperatorButtonWidget({@required this.operator, this.buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: FlatButton(
+          onPressed: () {},
+          color: buttonColor,
+          child: Container(
+            width: 55.0,
+            height: 55.0,
+            child: Icon(
+              operator,
+              size: 35.0,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
