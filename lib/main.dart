6b002771801b047c5cf839203317b90569053356
Nithin Rauj <nithindrake@gmail.com>
2019-09-18 22:24:49 +0530

    Defined the device orientation to be only portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app_page.dart';
 
 void main() => runApp(App());
@@ -6,6 +7,9 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       theme: ThemeData.dark(),
       title: 'Calcify',
